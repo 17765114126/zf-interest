4a394abfc050799c48317d9ec23aeabae38207b4
zhaofu <17765114126@163.com>
2021-04-19 20:52:29 +0800

    MybatisPlusInterceptor

--- interest/src/main/java/com/example/springboot/SpringbootApplication.java
+++ interest/src/main/java/com/example/springboot/SpringbootApplication.java
@@ -1,13 +1,14 @@
 package com.example.springboot;
 
-import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("com.example.springboot.mapper")
@@ -21,11 +22,12 @@ public class SpringbootApplication {
     }
 
     /**
-    *mybatis-plus分页使用配置
-    */
+     * mybatis-plus分页使用配置
+     */
     @Bean
-    public PaginationInterceptor paginationInterceptor() {
-        return new PaginationInterceptor();
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
     }
-
 }
